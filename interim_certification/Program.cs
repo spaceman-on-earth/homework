3bb0b2302af92e71b286516e94a1aa32e555bfc3
Alexander P <a.i.82@hotmail.com>
2023-06-09 22:05:21 +0200

    Added solution for 'Задача 66'

--- interim_certification/Program.cs
+++ interim_certification/Program.cs
@@ -56,13 +56,114 @@ namespace Recursion
             return naturalNumbers;
         }
 
+        // Для задачи 66.
+
+        public static int GetSumOfAscNatNumbers(int M, int N)
+        {
+            if (M <= N)
+            {
+                return (M + GetSumOfAscNatNumbers(M + 1, N));
+            }
+            else return 0;
+        }
+
+        public static int GetSumOfDescNatNumbers(int M, int N)
+        {
+            if (M >= N)
+            {
+                return (M + GetSumOfDescNatNumbers(M - 1, N));
+            }
+            else return 0;
+        }
+
+        public static int GetSumOfNatNumbersInRange()
+        {
+            int M, N;   // Границы промежутка натуральных чисел для суммирования.
+            int sum;    // Сумма натуральных чисел.
+            string? input;   // Строка пользовательского ввода.
+
+            while(true)
+            {
+                try
+                {   
+                    Write("\nType starting natural number and press ENTER: ");
+                    input = ReadLine();
+                    if (int.TryParse(input, out M))
+                    {
+                        if (M < 1)
+                        {
+                            WriteLine("Incorrect input! Try again.");
+                            continue;
+                        }
+                        else break;
+                    }
+                    else
+                    {
+                        WriteLine("Incorrect input! Try again.");
+                        continue;
+                    }
+                }
+                catch (OverflowException)
+                {
+                    WriteLine("Number is out of range!");
+                    WriteLine("It must be in [1, 2_147_483_647]. Try again.");
+                    continue;
+                }    
+            }
+            while(true)
+            {
+                try
+                {   
+                    Write("\nType ending natural number and press ENTER: ");
+                    input = ReadLine();
+                    if (int.TryParse(input, out N))
+                    {
+                        if (M < 1)
+                        {
+                            WriteLine("Incorrect input! Try again.");
+                            continue;
+                        }
+                        else break;
+                    }
+                    else
+                    {
+                        WriteLine("Incorrect input! Try again.");
+                        continue;
+                    }
+                }
+                catch (OverflowException)
+                {
+                    WriteLine("Number is out of range!");
+                    WriteLine("It must be in [1, 2_147_483_647]. Try again.");
+                    continue;
+                }    
+            }
+
+            if (M < N)
+            {
+                sum = GetSumOfAscNatNumbers(M, N);
+                return sum;
+            }
+            else if (M > N)
+            {
+                sum = GetSumOfDescNatNumbers(M, N);
+                return sum;
+            }
+            else return M;
+        }
+
         public static void Main()
         {
+            //WriteLine("----------");
+            //WriteLine("\nProcessing 'Задача 64':");
+            //var naturalNumbers = GetAllNatNumbersDesc();
+            //var print = String.Join(", ", naturalNumbers);
+            //WriteLine($"\nNatural numbers: {print}\n");
+
             WriteLine("----------");
             WriteLine("\nProcessing 'Задача 64':");
-            var naturalNumbers = GetAllNatNumbersDesc();
-            var print = String.Join(", ", naturalNumbers);
-            WriteLine($"\nNatural numbers: {print}\n");
+            int sum = GetSumOfNatNumbersInRange();
+            WriteLine($"\nSum of natural numbers on the interval: {sum}\n");
         }
     }
 }
